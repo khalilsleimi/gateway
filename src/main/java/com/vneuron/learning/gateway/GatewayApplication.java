901ca68f29878c1f86f75df9af0b9a5953c669b1
Khalil SLEIMI <khalil.sleimi@vneuron.com>
2021-09-24 10:42:18 +0100

    Add fallback request mapper

--- src/main/java/com/vneuron/learning/gateway/GatewayApplication.java
+++ src/main/java/com/vneuron/learning/gateway/GatewayApplication.java
@@ -2,12 +2,46 @@ package com.vneuron.learning.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerFilterFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .host("*.circuitbreaker.com")
+                        .filters(f -> f.circuitBreaker(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+    @Bean
+    public ReactiveResilience4JCircuitBreakerFactory factory()
+    {
+        return new ReactiveResilience4JCircuitBreakerFactory();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
     }
 
 }
